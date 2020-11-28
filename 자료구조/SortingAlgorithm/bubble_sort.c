ac4ec556fc9c1397dc0d63766fc76be7fad31ee5
yoojinhee03 <yoojinhee03@gmail.com>
2020-11-28 19:30:01 +0900

    Update bubble_sort.c

--- 자료구조/SortingAlgorithm/bubble_sort.c
+++ 자료구조/SortingAlgorithm/bubble_sort.c
@@ -1,24 +1,26 @@
 //버블정렬
 #include <stdio.h>
 
-void bubble_sort(int numArr[10],int count) {
+void bubble_sort(int numArr[10], int count) {
 	int temp;
-	for (int i = 0; i < count - 1; i++) {
-		for (int j = i+1; j < count; j++) {
-			if (numArr[i] > numArr[j]) {
-				temp = numArr[i]; numArr[i] = numArr[j]; numArr[j] = temp;
-			}
-		}
-	}
 	for (int i = 0; i < count; i++) {
-		printf("%d ", numArr[i]);
+		for (int j = 0; j < count - i - 1; j++) {
+			if (numArr[j] >= numArr[j + 1]) {
+				temp = numArr[j]; numArr[j] = numArr[j + 1]; numArr[j + 1] = temp;
+			}
+			for (int i = 0; i < count; i++) {
+				printf("%d ", numArr[i]);
+			}
+			printf("\n");
+		}
 	}
 }
 int main()
 {
 
 	//입력으로 변경
-	int numArr[10] = { 4, 2, 8, 9, 3, 7, 1, 10, 6, 5 };   // 정렬되지 않은 배열
+	int numArr[5] = { 4,3,1,5,2 };   // 정렬되지 않은 배열
+	//int numArr[10] = { 4, 2, 8, 9, 3, 7, 1, 10, 6, 5 };   // 정렬되지 않은 배열
 	int count = sizeof(numArr) / sizeof(int);
 	bubble_sort(numArr, count);
 
